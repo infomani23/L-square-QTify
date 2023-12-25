664223a7b4cfd3c147d1f2319c45c847af097a0f
Manikandan <dbamanikandan23@gmail.com>
2023-12-25 13:22:51 +0530

    herosection commit

--- qtify/src/Layout.jsx
+++ qtify/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import HeroSection from "./Components/HeroSection/HeroSection";
 import "./Layout.css";
 
 const Layout = () => {
     return (
       <div className="layout">
         <NavBar />
+        <HeroSection />
         </div>
         );
         }
