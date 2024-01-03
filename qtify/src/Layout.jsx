a2c6e930d59344c618f2da15584a72b92eadc03a
Manikandan <dbamanikandan23@gmail.com>
2024-01-03 15:55:21 +0530

    songs

--- qtify/src/Layout.jsx
+++ qtify/src/Layout.jsx
@@ -3,6 +3,9 @@ import NavBar from "./Components/NavBar/NavBar";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import TopAlbum from "./Components/topAlbum/TopAlbum";
 import NewAlbum from "./Components/newAlbum/NewAlbum";
+import AudioPlayer from "./Components/AudioPlayer/AudioPlayer";
+import Songs from "./Components/Songs/Songs";
+
 import "./Layout.css";
 const Layout = () => {
     return (
@@ -11,6 +14,8 @@ const Layout = () => {
         <HeroSection />
         <TopAlbum />
         <NewAlbum />
+        <Songs/>
+        <AudioPlayer />
         </div>
         );
         }
